b76805e4f14f8b5fdbff5a82dac6218be482f299
Yurii Dano <yuriidano@gmail.com>
2025-07-21 22:36:36 +0200

    feat(prisma): update schema.prisma

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,11 +1,17 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "../../../../prisma/prisma-client";
 
 
-
-export async function GET() {
-
-    const users = await prisma.user.findMany()
+export async function GET () {
+    const users = await prisma.user.findMany();
 
     return NextResponse.json(users)
+};
+
+export async function POST (req: NextRequest) {
+    const data = await req.json();
+
+    const user = await prisma.user.create({data});
+
+    return NextResponse.json(user)
 }
